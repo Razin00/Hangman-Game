f820a6fa185e9cf6a171916bd097af060032d981
Anonymous <121455872+Razin00@users.noreply.github.com>
2023-10-21 18:03:15 +0530

    Add files via upload

---
+++ Hangman Game/scripts/word-list.js
@@ -0,0 +1,166 @@
+const wordList = [
+   {
+    word: "ronaldo",
+    hint: "Without football, my life is nothing."
+   },
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings."
+    },
+    {
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life."
+    },
+    {
+        word: "mountain",
+        hint: "A large natural elevation of the Earth's surface."
+    },
+    {
+        word: "painting",
+        hint: "An art form using colors on a surface to create images or expression."
+    },
+    {
+        word: "anonymous",
+        hint: "Another word for unknown."
+    },
+    {
+        word: "hacker",
+        hint: "We break Security ,Not heart."
+    },
+    {
+        word: "astronomy",
+        hint: "The scientific study of celestial objects and phenomena."
+    },
+    {
+        word: "football",
+        hint: "A popular sport played with a spherical ball."
+    },
+    {
+        word: "history",
+        hint: "The study of past events and human civilization."
+    },
+    {
+        word: "camera",
+        hint: "A device used to capture and record images or videos."
+    },
+    {
+        word: "diamond",
+        hint: "A precious gemstone known for its brilliance and hardness."
+    },
+    {
+        word: "adventure",
+        hint: "An exciting or daring experience."
+    },
+    {
+        word: "science",
+        hint: "The systematic study of the structure and behavior of the physical and natural world."
+    },
+    {
+        word: "bicycle",
+        hint: "A human-powered vehicle with two wheels."
+    },
+    {
+        word: "sunset",
+        hint: "The daily disappearance of the sun below the horizon."
+    },
+    {
+        word: "coffee",
+        hint: "A popular caffeinated beverage made from roasted coffee beans."
+    },
+    {
+        word: "dance",
+        hint: "A rhythmic movement of the body often performed to music."
+    },
+    {
+        word: "galaxy",
+        hint: "A vast system of stars, gas, and dust held together by gravity."
+    },
+   {
+        word: "astronaut",
+        hint: "A person trained to travel and work in space."
+    },
+    {
+        word: "waterfall",
+        hint: "A cascade of water falling from a height."
+    },
+    {
+        word: "technology",
+        hint: "The application of scientific knowledge for practical purposes."
+    },
+    {
+        word: "virat",
+        hint: "The bat is not a toy ,it's a weapon."
+    },
+    {
+        word: "universe",
+        hint: "All existing matter, space, and time as a whole."
+    },
+    {
+        word: "vacation",
+        hint: "A period of time devoted to pleasure, rest, or relaxation."
+    },
+    {
+        word: "rainforest",
+        hint: "A dense forest characterized by high rainfall and biodiversity."
+    },
+    {
+        word: "theater",
+        hint: "A building or outdoor area in which plays, movies, or other performances are staged."
+    },
+    {
+        word: "telephone",
+        hint: "A device used to transmit sound over long distances."
+    },
+    {
+        word: "language",
+        hint: "A system of communication consisting of words, gestures, and syntax."
+    },
+    {
+        word: "desert",
+        hint: "A barren or arid land with little or no precipitation."
+    },
+    {
+        word: "sunflower",
+        hint: "A tall plant with a large yellow flower head."
+    },
+    {
+        word: "photography",
+        hint: "The art, process, or practice of creating images by recording light or other electromagnetic radiation."
+    },
+    {
+        word: "shadow",
+        hint: "A dark area or shape produced by an object blocking the light."
+    },
+    {
+        word: "secret",
+        hint: "Something kept hidden or unknown to others."
+    },
+    {
+        word: "curiosity",
+        hint: "A strong desire to know or learn something."
+    },
+    {
+        word: "unpredictable",
+        hint: "Not able to be foreseen or known beforehand; uncertain."
+    },
+    {
+        word: "illusion",
+        hint: "A false perception or belief; a deceptive appearance or impression."
+    },
+    {
+        word: "moonlight",
+        hint: "The light from the moon."
+    },
+    {
+        word: "vibrant",
+        hint: "Full of energy, brightness, and life."
+    },
+    {
+        word: "nostalgia",
+        hint: "A sentimental longing or wistful affection for the past."
+    },
+    {
+        word: "brilliant",
+        hint: "Exceptionally clever, talented, or impressive."
+    },
+];
